d7fe0074fef5e33007a0781375cdac0c2b3fb720
Eva Sánchez <122869904+EvaMsanchez@users.noreply.github.com>
2023-11-20 13:17:40 +0100

    first commit

---
+++ ejercicio01/Ejercicio01.java
@@ -0,0 +1,13 @@
+package es.studium.ejercicio01;
+
+public class Ejercicio01 
+{
+	/* HOLA MUNDO - Realizar un programa que muestre por pantalla la frase “Hola, mundo”. */
+	
+	public static void main(String[] args) 
+	{
+		// ESCRIBIR "Hola, mundo"
+		System.out.println("Hola, mundo");
+	}
+	
+}
